14bcabc38def4cad91ad1bbff83dd6e40bfc551c
Deepak <deepakkushwaha5945@gmail.com>
2026-06-14 13:58:10 +0530

    delay added

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -66,7 +66,7 @@ export default function Header() {
             {/* Mobile Menu */}
             <div className={`absolute top-[59px] sm:hidden block
                  ${show ? 'translate-x-[-100%]' : 'translate-x-0'} 
-                transition-all duration-700
+                transition-all duration-800
                 w-full h-screen bg-white shadow`}>
 
                 <ul className="flex flex-col gap-3  m-10">
