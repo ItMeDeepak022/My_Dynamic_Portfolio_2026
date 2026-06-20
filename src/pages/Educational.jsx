73ec6c5d439f4141a3ef3109fbfbdfbcccca9e6e
Deepak <deepakkushwaha5945@gmail.com>
2026-06-20 06:28:20 +0530

    CGPA addeed

--- src/pages/Educational.jsx
+++ src/pages/Educational.jsx
@@ -15,7 +15,7 @@ const educationData = [
         title: "B.Tech (Computer Science)",
         school: "RR Institute of Modern Technology , Lucknow UP",
         year: "2022 - 2026",
-        percentage: "Final Year",
+        percentage: " CGPA - 7.82",
     },
 ];
 
